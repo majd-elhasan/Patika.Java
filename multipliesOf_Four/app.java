a6130fdd14b703dd955c7508297f8a9fd7fd40c5
Mecid(Majd ) elhasan <halabylilabad2@gmail.com>
2022-04-28 22:04:04 +0300

    Create app.java

---
+++ multipliesOf_Four/app.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class app {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Sınır sayısına kadar olan sayılardan (4)'ün katları olan sayıları toplayalım :) .");
+        
+        int limitNum = input();
+        int sum =0;
+        for(int i = 1 ; i <= limitNum ; i++){
+            if(i%4==0) sum+=i;
+        }
+         System.out.println("toplam : "+ sum);
+
+        scanner.close();
+    }
+    static int input(){
+        Scanner scanner = new Scanner(System.in);
+        try {
+            System.out.print("sınır sayısını giriniz : ");
+            int limitInput = scanner.nextInt();
+            return limitInput;
+
+        } catch (Exception e) {
+            System.out.println("Rakam girmelisiniz !");
+            input();
+        }
+        scanner.close();
+        return 0;
+    }
+}
