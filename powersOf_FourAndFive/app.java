a5b696c025a10e460958788d236b76c7ef71806d
Mecid(Majd ) elhasan <halabylilabad2@gmail.com>
2022-04-28 22:36:20 +0300

    Update app.java

--- powersOf_FourAndFive/app.java
+++ powersOf_FourAndFive/app.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 public class app {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Girilen sayıya kadar olan (4) ve (5)'in küvvetlerini yazdıralım :) .");
+        System.out.println("Girilen sayıya kadar olan (4) ve (5)'in kuvvetlerini yazdıralım :) .");
         
         int limitNum = input();
-        System.out.println("4'ün küvvetleri     5'in küvvetleri");
+        System.out.println("4'ün kuvvetleri     5'in kuvvetleri");
         for(int i = 1 ,j = 1; i <= limitNum ; i*=4,j*=5){
             System.out.print(i);
             if(j <= limitNum)
