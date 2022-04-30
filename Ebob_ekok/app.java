85e1b169b31499e4036961c05c224460d8c0241c
Mecid(Majd ) elhasan <halabylilabad2@gmail.com>
2022-04-30 11:56:29 +0300

    Update app.java

--- Ebob_ekok/app.java
+++ Ebob_ekok/app.java
@@ -1,5 +1,6 @@
 import java.lang.reflect.WildcardType;
 import java.util.Scanner;
+import java.util.concurrent.CountDownLatch;
 
 public class app {
     public static void main(String[] args) {
@@ -18,17 +19,22 @@ public class app {
                 b = scanner.nextInt();
                 int ebob = 1;
                 int smallnum = a < b ? a : b;
+                System.out.println("==================");
                 // for(int i= 1 ;i<= smallnum ; i++){
                 //     if(a % i == 0 && b % i == 0)
                 //         ebob = i;
                 // }
-                int i = 1;
-                while(i <= smallnum){
-                    if(a % i == 0 && b % i == 0)
-                        ebob = i;
-                    i++;
+                
+                int k = smallnum;
+                while(k >= 1){
+                    if(a % k == 0 && b % k == 0)
+                    {
+                        ebob = k;
+                        break;
+                    }
+                    k--;
                 }
-                System.out.println("EBOB : "+ebob);
+                System.out.println("EBOB : "+ebob );
             break;
             case 2:
                 System.out.print("ilk sayı giriniz : ");
@@ -37,6 +43,7 @@ public class app {
                 b = scanner.nextInt();
                 int bignum = a > b ? a : b;
                 int ekok = bignum;
+                System.out.println("==================");
                 if(!(a ==0 || b==0))
                 {
                     // for(int j = bignum ;j <= a*b ;j++){
