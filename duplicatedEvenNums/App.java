baa2b7d928ffb3d011f2ec2b6f92492e7d473876
Mecid(Majd ) elhasan <halabylilabad2@gmail.com>
2022-05-03 16:09:17 +0300

    duplicatedNums

---
+++ duplicatedEvenNums/App.java
@@ -0,0 +1,27 @@
+class App{
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5,5,6,7,1,2,8};
+        duplicatedNums(arr);
+    }
+    static void duplicatedNums(int[] arr){
+        for(int i = 1 ; i < arr.length ; i++){
+            for(int j = 0 ; j < i; j++){
+                if(i != j && arr[j] == arr[i]){
+                    if(!isDuplicated(arr, arr[j]))
+                        System.out.print(arr[j] + " ");
+                }    
+            }
+        }
+    }
+    static Boolean isDuplicated(int[] arr,int x){
+        int times = 0;
+        for(int i = 1 ; i < arr.length ; i++)
+                if(x == arr[i] && x%2==0)
+                {
+                    times++;
+                }
+        if(times < 1)
+        return true;
+    return false;
+    }
+}
